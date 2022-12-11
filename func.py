c064f3b42462a05cb26e4d735b150a794ae5668a
kitdim <kitaykin9808@gmail.com>
2022-12-11 20:55:03 +0300

    Добавил поддержку загрузки и сохранения данных

---
+++ func.py
@@ -0,0 +1,17 @@
+import pickle
+import os
+
+def Load():
+    with open(FILENAME, 'rb') as file:
+        if os.stat(FILENAME).st_size == 0:
+            days = 0
+        else:
+            days = pickle.load(file)
+
+    return days
+
+def Save(day_without_smoking):
+    with open(FILENAME, 'wb') as file:
+        pickle.dump(day_without_smoking, file)
+
+FILENAME = "data\\days.dat"
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+from func import *
+
 print("The KillSmoke shows the number of days without cigarettes.")
 
 day_without_smoking = Load()
-answer = input("Did you smoke today?y/n")
+answer = input("Did you smoke today (y/n)?: ")
+
+print(f"Days without smokking {day_without_smoking}")
 
 if(answer == 'y'):
     day_without_smoking = 0
+day_without_smoking += 1
 
 Save(day_without_smoking)
-
-def Load():
-    return 
-
-def Save(day_without_smoking):
-    pass
