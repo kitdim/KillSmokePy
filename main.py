c291e5fb19a20427132e8d1934b2d4ef1968f4be
kitdim <kitaykin9808@gmail.com>
2022-12-18 15:59:23 +0300

    перенес вывод и поправил условие , теперь 0 присваеватся правильно

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ print("The KillSmoke shows the number of days without cigarettes.")
 day_without_smoking = Load()
 answer = input("Did you smoke today (y/n)?: ")
 
-print(f"Days without smokking {day_without_smoking}")
-
 if(answer == 'y'):
     day_without_smoking = 0
-day_without_smoking += 1
+else:
+    day_without_smoking += 1
 
 Save(day_without_smoking)
+
+print(f"Days without smokking {day_without_smoking}")
+#TODO Отредакатирова загрузку, скорее всего ошибка в расположении функций Load и Save
